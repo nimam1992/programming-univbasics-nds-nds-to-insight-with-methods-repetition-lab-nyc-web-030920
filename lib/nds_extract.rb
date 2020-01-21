d8f5bbcd375ed2f591e6dc7425c2d2176f10183e
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-21 04:46:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,5 +53,5 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  result
+  grand_total
 end
